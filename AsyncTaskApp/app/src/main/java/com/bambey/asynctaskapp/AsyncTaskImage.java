d3a9e1d17fac33d5e5af64663b1ec1a6b6f04a0b
kaya1995 <aissatou.faye2@uadb.edu.sn>
2019-12-10 08:44:17 +0000

    application sur les AsyncTask

---
+++ AsyncTaskApp/app/src/main/java/com/bambey/asynctaskapp/AsyncTaskImage.java
@@ -0,0 +1,79 @@
+package com.bambey.asynctaskapp;
+
+import android.os.AsyncTask;
+import android.graphics.Bitmap;
+
+import android.app.Activity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import androidx.fragment.app.FragmentActivity;
+import com.bambey.asynctaskapp.R;
+
+public class AsyncTaskImage extends AsyncTask<String, Integer, Bitmap> {
+
+    private Activity activity;
+
+    public AsyncTaskImage(FragmentActivity myActivity) {
+        activity = myActivity;
+    }
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        Log.i("hello1", "hello1");
+        publishProgress(1);
+        try {
+            URL url = new URL(strings[0]);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            if (con.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new Exception("Failed to connect");
+            }
+            InputStream is = con.getInputStream();
+            publishProgress(0);
+            Bitmap bitmap = BitmapFactory.decodeStream(is);
+            is.close();
+            return bitmap;
+        } catch (Exception e) {
+            Log.e("Image", "Failed to load image", e);
+            Log.e("error", e.getMessage());
+        }
+
+        return null;
+    }
+
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        TextView tv = (TextView) activity.findViewById(R.id.tv_loading);
+        if (values[0] == 1) {
+            tv.setText("Loading...");
+        } else {
+            tv.setText("");
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap img) {
+
+        Log.i("hello4", "hello4");
+        ImageView iv = (ImageView) activity.findViewById(R.id.remote_image);
+        Log.i("hello5", "hello5");
+        if (iv != null && img != null) {
+            Log.i("hello6", "hello6");
+            iv.setImageBitmap(img);
+        }
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+}
